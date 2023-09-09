11c88ceb36259b461baf4c86b13d8ea8b6512b66
dutianze <dutianzer@gmail.com>
2023-09-09 09:56:24 +0900

    PostCreateCmd record

--- src/main/java/io/github/dutianze/memo/controller/dto/PostCreateCmd.java
+++ src/main/java/io/github/dutianze/memo/controller/dto/PostCreateCmd.java
@@ -3,7 +3,6 @@ package io.github.dutianze.memo.controller.dto;
 import io.github.dutianze.memo.entity.Post;
 import io.github.dutianze.memo.entity.PostTag;
 import io.github.dutianze.memo.entity.Tag;
-import lombok.Data;
 
 import java.util.Collection;
 import java.util.List;
@@ -14,16 +13,13 @@ import java.util.stream.Stream;
  * @author dutianze
  * @date 2023/9/9
  */
-@Data
-public class PostCreateCmd {
+public record PostCreateCmd(
+        String title,
+        String backgroundImage,
+        String content,
+        List<Long> tagIds
+) {
 
-    private String title;
-
-    private String backgroundImage;
-
-    private String content;
-
-    private List<Long> tagIds;
 
     public Post newPost() {
         return new Post(this.title, this.backgroundImage, this.content);
