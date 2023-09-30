772b08d1df845899873b183271513bb890068b98
dutianze <dutianzer@gmail.com>
2023-09-30 15:12:24 +0900

    pack

--- src/main/resources/static/_next/static/EpRNuP5xFEUwW6GBUmtXN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST=function(e){return{__rewrites:{afterFiles:[{has:e,source:"/api/:path*",destination:e}],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-cec4f1a3c48010b1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]}}(void 0),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
